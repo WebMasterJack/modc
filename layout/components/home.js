10f41640400587556de576f19204d242c14fc787
WebMasterJack <elizarov.maxim.2002@gmail.com>
2021-10-08 22:42:00 +0300

    Модуль Д: vue-route and components

---
+++ layout/components/home.js
@@ -0,0 +1,31 @@
+export default {
+    name: 'Home',
+    data() {
+      return {
+        "items": [
+            {
+                "name": "Kazan",
+                "iata": "KZN"
+            },
+            {
+                "name": "Sheremetyevo",
+                "iata": "SVO"
+            },
+            {
+                "name": "Pulkovo",
+                "iata": "LED"
+            },
+            {
+                "name": "Sochi",
+                "iata": "AER"
+            },
+            {
+                "name": "Saransk",
+                "iata": "SKX"
+            }
+        ]
+      };
+    },
+    
+    template:`<nav class="navbar navbar-expand-lg navbar-light bg-light"> <div class="container"> <img src="assets/images/logo.png" class="logo test-0-logo" alt="logo"> <div class="navbar-collapse"> <ul class="navbar-nav ml-auto"> <li class="nav-item active"> <a class="nav-link test-0-nav-1" href="#">Promotions</a> </li> <li class="nav-item"> <a class="nav-link test-0-nav-2" href="#">Search</a> </li> <li class="nav-item"> <a class="nav-link test-0-nav-3" href="#">Check-in</a> </li> <li class="nav-item"> <a class="nav-link test-0-nav-4" href="login.html">Personal area</a> </li> </ul> </div> </div> </nav> <div class="container"> <section class="mt-5"> <h2 class="mb-4">Flight search</h2> <form action="./search"> <div class="row"> <div class="col-12 col-sm-6 col-lg-4 col-xl-3 pr-lg-0"> <select class="form-control test-0-fd w-100" v-model="selected" > <option selected>Откуда</option> <option v-for="flight in items" :value="flight.iata" >{{flight.name}}</option> </select> </div> <div class="col-12 col-sm-6 col-lg-4 mt-3 col-xl-3 mt-sm-0 pr-lg-0"> <select class="form-control test-0-fd w-100" v-model="selected" > <option selected value="Where" >Куда</option> <option v-for="flight in items" :value="flight.iata" >{{flight.name}}</option> </select> </div> <div class="col-12 col-sm-6 col-lg-2 mt-3 col-xl-2 mt-lg-0 pr-lg-0"> <input type="text" class="form-control test-0-fdt" placeholder="Departing"> <div class="invalid-feedback">Error message</div> </div> <div class="col-12 col-sm-6 col-lg-2 mt-3 col-xl-2 mt-lg-0 pr-xl-0"> <input type="text" class="form-control test-0-fat" placeholder="Returning"> <div class="invalid-feedback">Error message</div> </div> <div class="col-12 col-sm-6 col-lg-2 mt-3 col-xl-1 mt-lg-0 pr-xl-0"> <select class="form-control test-0-fnp"> <option value="1">1</option> <option value="2">2</option> <option value="3">3</option> <option value="4">4</option> <option value="5">5</option> <option value="6">6</option> <option value="7">7</option> <option value="8">8</option> </select> <div class="invalid-feedback">Error message</div> </div> <div class="col-12 col-xl-1 px-2 mt-3 mt-xl-0"> <button class="btn btn-success form-control test-0-fbs">Search</button> </div> </div> </form> </section> <section class="mt-5"> <h2 class="mb-4">Promotions</h2> <div class="row"> <div class="col-12 col-md-6 col-lg-3"> <div class="card"> <img src="assets/images/placeholder.jpg" class="card-img-top test-0-ai" alt="..."> <div class="card-body"> <h5 class="card-title test-0-an">Promotion 1</h5> <p class="card-text test-0-ad">Promotion text</p> <a href="#" class="btn btn-primary test-0-abm">More</a> </div> </div> </div> <div class="col-12 col-md-6 col-lg-3"> <div class="card"> <img src="assets/images/placeholder.jpg" class="card-img-top test-0-ai" alt="..."> <div class="card-body"> <h5 class="card-title test-0-an">Promotion 1</h5> <p class="card-text test-0-ad">Promotion text</p> <a href="#" class="btn btn-primary test-0-abm">More</a> </div> </div> </div> <div class="col-12 col-md-6 col-lg-3"> <div class="card"> <img src="assets/images/placeholder.jpg" class="card-img-top test-0-ai" alt="..."> <div class="card-body"> <h5 class="card-title test-0-an">Promotion 1</h5> <p class="card-text test-0-ad">Promotion text</p> <a href="#" class="btn btn-primary test-0-abm">More</a> </div> </div> </div> <div class="col-12 col-md-6 col-lg-3"> <div class="card"> <img src="assets/images/placeholder.jpg" class="card-img-top test-0-ai" alt="..."> <div class="card-body"> <h5 class="card-title test-0-an">Promotion 1</h5> <p class="card-text test-0-ad">Promotion text</p> <a href="#" class="btn btn-primary test-0-abm">More</a> </div> </div> </div> </div> </section> <section class="mt-5"> <h2 class="mb-4">Subscribe</h2> <form action="#"> <div class="row"> <div class="col-12 col-md-6"> <input type="email" class="form-control test-0-sie" placeholder="example@domain.com"> </div> <div class="col-12 col-md-6"> <button class="btn btn-success w-100 test-0-sbs">Subscribe</button> </div> </div> </form> </section> </div> <footer class="mt-5"> <div class="container"> <div class="row"> <div class="col-3"> <span class="test-0-phone">8 (800) 100-10-10</span> </div> <div class="col-7 offset-2"> <div class="d-flex justify-content-between"> <a href="http://xxxxxx-m1.wsr.ru">Home</a> <a href="http://xxxxxx-m1.wsr.ru/login">Log in</a> <a href="http://xxxxxx-m1.wsr.ru/register">Registration</a> <a href="http://xxxxxx-m1.wsr.ru/contact">Contact</a> <a href="http://xxxxxx-m1.wsr.ru/news">News</a> <a href="http://xxxxxx-m1.wsr.ru/return">Return</a> <a href="http://xxxxxx-m1.wsr.ru/feedback">Feedback</a> </div> </div> </div> </div> </footer>`
+}
--- layout/vue.js
+++ layout/vue.js
@@ -1,167 +1,16 @@
+import Search from './components/search.js';
+import Home from './components/home.js';
 
 
 
 
 
-const search = {
-    template: '<div class="container">        <section class="mt-5">            <div class="d-flex justify-content-between align-items-start">                <h2 class="mb-4">                    We found the following flights                </h2>                <a href="index.html" class="btn btn-sm btn-secondary test-4-bback">Back</a>            </div>            <h5 class="mt-4 mb-3">                <span class="test-4-fif1">Saransk</span>                ->                <span class="test-4-fit1">Moscow</span>            </h5>            <table class="table table-hover table-bordered">                <tr>                    <th>Flight</th>                    <th>Aircraft</th>                    <th>Date and time of departure</th>                    <th>Arrival time</th>                    <th>Flight time</th>                    <th>Cost</th>                </tr>                <tr class="selected">                    <td class="test-4-fn">FC 2144</td>                    <td class="test-4-at">Sukhoi Superjet 100</td>                    <td>                        <span class="test-4-dd">10-02-2021</span>                        at                        <span class="test-4-dt">08:00</span>                    </td>                    <td class="test-4-aatime">09:30</td>                    <td class="test-4-ft">                        <span class="test-4-fhour">1</span>h                        <span class="test-4-fminutes">30</span>min                    </td>                    <td class="test-4-fp">8000</td>                </tr>                <tr>                    <td class="test-4-fn">FC 2162</td>                    <td class="test-4-at">Sukhoi Superjet 100</td>                    <td>                        <span class="test-4-dd">10-02-2021</span>                        at                        <span class="test-4-dt">12:00</span>                    </td>                    <td class="test-4-aatime">13:30</td>                    <td class="test-4-ft">                        <span class="test-4-fhour">1</span>h                        <span class="test-4-fminutes">30</span>min                    </td>                    <td class="test-4-fp">8000</td>                </tr>            </table>            <h5 class="mt-4 mb-3">                <span class="test-4-fif2">Moscow</span>                ->                <span class="test-4-fit2">Saransk</span>            </h5>            <table class="table table-hover table-bordered">                <tr>                    <th>Flight</th>                    <th>Aircraft</th>                    <th>Date and time of departure</th>                    <th>Arrival time</th>                    <th>Flight time</th>                    <th>Cost</th>                </tr>                <tr>                    <td class="test-4-fn">FC 2143</td>                    <td class="test-4-at">Bombardier CRJ200</td>                    <td>                        <span class="test-4-dd">11-02-2021</span>                        at                        <span class="test-4-dt">06:00</span>                    </td>                    <td class="test-4-aatime">07:30</td>                    <td class="test-4-ft">                        <span class="test-4-fhour">1</span>h                        <span class="test-4-fminutes">30</span>min                    </td>                    <td class="test-4-fp">9000</td>                </tr>                <tr class="selected">                    <td class="test-4-fn">FC 2161</td>                    <td class="test-4-aatime">Bombardier CRJ200</td>                    <td>                        <span class="test-4-dd">11-02-2021</span>                        at                        <span class="test-4-dt">10:00</span>                    </td>                    <td class="test-4-at">11:30</td>                    <td class="test-4-ft">                        <span class="test-4-fhour">1</span>h                        <span class="test-4-fminutes">30</span>min                    </td>                    <td class="test-4-fp">9000</td>                </tr>            </table>            <a href="booking.html" class="btn btn-primary test-4-bgobook mt-4">Go to booking</a>        </section>    </div>'
-    
-  }
-  
 
-
-const Airport = {
-    data (){
-        
-        
-        return {
-            
-                valueFrom:'Kazan',
-                valueTo:'Sheremetyevo',
-            
-                "items": [
-                    {
-                        "name": "Kazan",
-                        "iata": "KZN"
-                    },
-                    {
-                        "name": "Sheremetyevo",
-                        "iata": "SVO"
-                    },
-                    {
-                        "name": "Pulkovo",
-                        "iata": "LED"
-                    },
-                    {
-                        "name": "Sochi",
-                        "iata": "AER"
-                    },
-                    {
-                        "name": "Saransk",
-                        "iata": "SKX"
-                    }
-                ]
-            
-        }
-        
-        },
-      
-};
-
-
-
-// const ChangeFrom = {
-//     data() {
-//       return {
-//         "flights_to": [
-//             {
-//                 "flight_id": 2,
-//                 "flight_code": "FP1200",
-//                 "from": {
-//                     "city": "Saransk",
-//                     "airport": "Saransk",
-//                     "iata": "SKX",
-//                     "date": "2021-02-10",
-//                     "time": "12:00"
-//                 },
-//                 "to": {
-//                     "city": "Moscow",
-//                     "airport": "Sheremetyevo",
-//                     "iata": "SVO",
-//                     "date": "2021-02-10",
-//                     "time": "13:35"
-//                 },
-//                 "cost": 9500,
-//                 "availability": 156
-//             },
-//             {
-//                 "flight_id": 14,
-//                 "flight_code": "FP 1201",
-//                 "from": {
-//                     "city": "Saransk",
-//                     "airport": "Saransk",
-//                     "iata": "SKX",
-//                     "date": "2021-02-10",
-//                     "time": "08:35"
-//                 },
-//                 "to": {
-//                     "city": "Moscow",
-//                     "airport": "Sheremetyevo",
-//                     "iata": "SVO",
-//                     "date": "2021-02-10",
-//                     "time": "10:05"
-//                 },
-//                 "cost": 10500,
-//                 "availability": 156
-//             }
-//         ],
-        
-//       }
-//     }
-//   };
-//   const ChangeTo = {
-//       data(){
-//           return {
-//             "flights_back": [
-//                 {
-//                     "flight_id": 1,
-//                     "flight_code": "FP 2100",
-//                     "from": {
-//                         "city": "Moscow",
-//                         "airport": "Sheremetyevo",
-//                         "iata": "SVO",
-//                         "date": "2020-10-10",
-//                         "time": "08:35"
-//                     },
-//                     "to": {
-//                         "city": "Saransk",
-//                         "airport": "Saransk",
-//                         "iata": "SKX",
-//                         "date": "2020-10-10",
-//                         "time": "10:05"
-//                     },
-//                     "cost": 10500,
-//                     "availability": 156
-//                 },
-//                 {
-//                     "flight_id": 13,
-//                     "flight_code": "FP 2101",
-//                     "from": {
-//                         "city": "Moscow",
-//                         "airport": "Sheremetyevo",
-//                         "iata": "SVO",
-//                         "date": "2020-10-10",
-//                         "time": "12:00"
-//                     },
-//                     "to": {
-//                         "city": "Saransk",
-//                         "airport": "Saransk",
-//                         "iata": "SKX",
-//                         "date": "2020-10-10",
-//                         "time": "13:35"
-//                     },
-//                     "cost": 12500,
-//                     "availability": 156
-//                 }
-//             ],
-//           }
-//       }
-//   };
-
-//   Vue.createApp(Change).mount('#changeFrom');
-//   Vue.createApp(Change).mount('#changeTo');
-// Vue.createApp(search).mount('.search');
-
-
-
-// Vue.createApp(search).mount('#search');
 const NotFoundComponent = { template: '<p>Страница не найдена</p>' }
-const Home = { template: '<div>Home</div>' }
-const About = { template: '<div>About</div>' }
-
 const routes = [
     { path: '/', component: Home },
-    { path: '/search', component: search }
+    { path: '/search', component: Search },
+    
   ]
   
 
@@ -169,12 +18,12 @@ const routes = [
 let app=Vue.createApp({});
 const router = VueRouter.createRouter({
     // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-    history: VueRouter.createWebHashHistory(),
+    history: VueRouter.createWebHistory('/layout/'),
     routes, // short for `routes: routes`
   })
   app.use(router); 
 app.mount('#app')
-// Vue.createApp(Airport).mount('#air');
+Vue.createApp(Airport).mount('#air');
 
   // 3. Создаём экземпляр маршрутизатора и передаём маршруты в опции `routes`
   // Вы можете передавать и дополнительные опции, но пока не будем усложнять.
