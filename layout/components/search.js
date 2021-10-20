71b6c4b2af2c1b22b68f8db1263b49b94394a037
Your Name <you@example.com>
2021-10-20 13:22:28 +0300

    Complete fetch API vuejs

--- layout/components/search.js
+++ layout/components/search.js
@@ -1,68 +1,69 @@
 export default {
     name: 'Search',
-    async data() {
+    data() {
         
       console.log(321);
-      let {ffrom,fto,datefrom,datereturn,pgr}=this;
-      console.log(`From:${ffrom}\n To:${fto}\n Date From:${datefrom}\n Date Return:${datereturn}\n Passengers:${pgr}`);
-      let url = `http://modc/fnc-master/public/api/flight?from=${ffrom}&to=${fto}&date1=${datefrom}&date2=${datereturn}&passengers=${pgr} `;
+//       let {ffrom,fto,datefrom,datereturn,pgr}=this;
+//       console.log(`From:${ffrom}\n To:${fto}\n Date From:${datefrom}\n Date Return:${datereturn}\n Passengers:${pgr}`);
+//       let url = `http://modc/fnc-master/public/api/flight?from=${ffrom}&to=${fto}&date1=${datefrom}&date2=${datereturn}&passengers=${pgr} `;
       
-let response = await fetch(url);
+// let response = await fetch(url);
 
-let commits = await response.json(); // читаем ответ в формате JSON
-//GET response
-// http://modc/fnc-master/public/api/flight?from=SKX&to=SVO&date1=2021-02-10&date2=2021-02-11&passengers=1
-let data =  commits.data;
-let {flights_back,flights_to}=data;
+// let commits = await response.json(); // читаем ответ в формате JSON
+// //GET response
+// // http://modc/fnc-master/public/api/flight?from=SKX&to=SVO&date1=2021-02-10&date2=2021-02-11&passengers=1
+// let data =  commits.data;
+// let {flights_back,flights_to}=data;
 
-let firstPostFromCity=flights_back[0].from.city;
-console.log(firstPostFromCity);
+// let firstPostFromCity=flights_back[0].from.city;
+// console.log(firstPostFromCity);
 
     return {
-      count: await this.getComputedFlights(),
-      
-      firstFromCity: await this.firstPostFromCity,
+        flights_back:null,
+        flights_to:null
     };
       //vue-async-data
    
     },
    
-    methods:{
-        
-      async getComputedFlights() {
-        let {ffrom,fto,datefrom,datereturn,pgr}=this;
-        console.log(`From:${ffrom}\n To:${fto}\n Date From:${datefrom}\n Date Return:${datereturn}\n Passengers:${pgr}`);
-        let url = `http://modc/fnc-master/public/api/flight?from=${ffrom}&to=${fto}&date1=${datefrom}&date2=${datereturn}&passengers=${pgr} `;
+    created() {
+    // Simple GET request using fetch
+    let {ffrom,fto,datefrom,datereturn,pgr}=this;
+    console.log(`From:${ffrom}\n To:${fto}\n Date From:${datefrom}\n Date Return:${datereturn}\n Passengers:${pgr}`);
+    let url = `http://modc/fnc-master/public/api/flight?from=${ffrom}&to=${fto}&date1=${datefrom}&date2=${datereturn}&passengers=${pgr} `;
 
-        
-        let response = await fetch(url);
-        let commits = await response.json(); // читаем ответ в формате JSON
-        let data =  commits.data;
-        
-        // let firstPostToCity
-        // let firstPostFromFlightCode
-        // let firstPostToFlightCode
-        // let firstPostFromTime
-        // let firstPostToTime
-        // let firstPostFromDate
-        // let firstPostToDate
-        let {flights_back,flights_to}=data;
-        console.log(flights_back,flights_to);
-        
-        return {flights_back,flights_to};
-      }
+    
+    // let response = await fetch(url);
+    // let commits = await response.json(); // читаем ответ в формате JSON
+    // let data =  commits.data;
+    
+    // let firstPostToCity
+    // let firstPostFromFlightCode
+    // let firstPostToFlightCode
+    // let firstPostFromTime
+    // let firstPostToTime
+    // let firstPostFromDate
+    // let firstPostToDate
+    fetch(url)
+      .then(response => response.json())
+      .then((commits) => {
+          console.log(commits);
+          this.flights_back = commits.data.flights_back;
+          this.flights_to = commits.data.flights_to;
+      });
   },
    props:['name','ffrom','fto','datefrom','datereturn','pgr'],
 
     template: `<div class="container">
     <p> {{ffrom}} {{fto}} {{datefrom}} {{datereturn}} {{pgr}} {{testa}}</p>
-    {{count}} {{firstFromCity}}
+    
     <section class="mt-5">
         <div class="d-flex justify-content-between align-items-start">
             <h2 class="mb-4"> We found the following flights </h2> <a href="./"
                 class="btn btn-sm btn-secondary test-4-bback">Back</a>
         </div>
-        <h5 class="mt-4 mb-3"> <span class="test-4-fif1">Saransk</span> -> <span class="test-4-fit1">Moscow</span>
+
+        <h5 class="mt-4 mb-3"> <span class="test-4-fif2">{{flights_to[0].from.city}}</span> -> <span class="test-4-fit2">{{flights_to[0].to.city}}</span>
         </h5>
         <table class="table table-hover table-bordered">
             <tr>
@@ -73,55 +74,42 @@ console.log(firstPostFromCity);
                 <th>Flight time</th>
                 <th>Cost</th>
             </tr>
-            <tr class="selected">
-                <td class="test-4-fn">FC 2144</td>
+            <tr v-for="flight in flights_to">
+                <td class="test-4-fn">{{flight.flight_code}}</td>
                 <td class="test-4-at">Sukhoi Superjet 100</td>
-                <td> <span class="test-4-dd">10-02-2021</span> at <span class="test-4-dt">08:00</span> </td>
-                <td class="test-4-aatime">09:30</td>
+                <td> <span class="test-4-dd">{{flight.from.date}}</span> at <span class="test-4-dt">{{flight.from.time}}</span> </td>
+                <td class="test-4-aatime">{{flight.to.time}}</td>
                 <td class="test-4-ft"> <span class="test-4-fhour">1</span>h <span
                         class="test-4-fminutes">30</span>min </td>
-                <td class="test-4-fp">8000</td>
+                <td class="test-4-fp">{{flight.cost}}</td>
             </tr>
+        </table> 
+        <h5 class="mt-4 mb-3"> <span class="test-4-fif1">{{flights_back[0].from.city}}</span> -> <span class="test-4-fit1">{{flights_back[0].to.city}}</span>
+        </h5>
+        <table class="table table-hover table-bordered">
             <tr>
-                <td class="test-4-fn">FC 2162</td>
+                <th>Flight</th>
+                <th>Aircraft</th>
+                <th>Date and time of departure</th>
+                <th>Arrival time</th>
+                <th>Flight time</th>
+                <th>Cost</th>
+            </tr>
+            <tr v-for="flight in flights_back">
+                <td class="test-4-fn">{{flight.flight_code}}</td>
                 <td class="test-4-at">Sukhoi Superjet 100</td>
-                <td> <span class="test-4-dd">10-02-2021</span> at <span class="test-4-dt">12:00</span> </td>
-                <td class="test-4-aatime">13:30</td>
+                <td> <span class="test-4-dd">{{flight.from.date}}</span> at <span class="test-4-dt">{{flight.from.time}}</span> </td>
+                <td class="test-4-aatime">{{flight.to.time}}</td>
                 <td class="test-4-ft"> <span class="test-4-fhour">1</span>h <span
                         class="test-4-fminutes">30</span>min </td>
-                <td class="test-4-fp">8000</td>
+                <td class="test-4-fp">{{flight.cost}}</td>
             </tr>
+
+
+
         </table>
-        <h5 class="mt-4 mb-3"> <span class="test-4-fif2">Moscow</span> -> <span class="test-4-fit2">Saransk</span>
-        </h5>
-        <table class="table table-hover table-bordered">
-            <tr>
-                <th>Flight</th>
-                <th>Aircraft</th>
-                <th>Date and time of departure</th>
-                <th>Arrival time</th>
-                <th>Flight time</th>
-                <th>Cost</th>
-            </tr>
-            <tr>
-                <td class="test-4-fn">FC 2143</td>
-                <td class="test-4-at">Bombardier CRJ200</td>
-                <td> <span class="test-4-dd">11-02-2021</span> at <span class="test-4-dt">06:00</span> </td>
-                <td class="test-4-aatime">07:30</td>
-                <td class="test-4-ft"> <span class="test-4-fhour">1</span>h <span
-                        class="test-4-fminutes">30</span>min </td>
-                <td class="test-4-fp">9000</td>
-            </tr>
-            <tr class="selected">
-                <td class="test-4-fn">FC 2161</td>
-                <td class="test-4-aatime">Bombardier CRJ200</td>
-                <td> <span class="test-4-dd">11-02-2021</span> at <span class="test-4-dt">10:00</span> </td>
-                <td class="test-4-at">11:30</td>
-                <td class="test-4-ft"> <span class="test-4-fhour">1</span>h <span
-                        class="test-4-fminutes">30</span>min </td>
-                <td class="test-4-fp">9000</td>
-            </tr>
-        </table> <a href="booking.html" class="btn btn-primary test-4-bgobook mt-4">Go to booking</a>
+        
+        <a href="booking.html" class="btn btn-primary test-4-bgobook mt-4">Go to booking</a>
     </section>
 </div>`
 }
